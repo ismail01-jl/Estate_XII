6c7fc38ad725e357dddba5dbac869194e0c9ef11
ismail01-jl <jellitiismail2002@gmail.com>
2024-07-08 20:12:06 +0100

    Create Pages and Routes then Create Header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Signin from './pages/Signin';
+import SignUp from './pages/Signup';
+import Signout from './pages/Signout';
+import Profile from './pages/Profile';
+import About from './pages/About';
+import Header from './components/Header';
+
 export default function App() {
-  return (
-    <div>App</div>
+  return(
+    <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/signin' element={<Signin />} />
+      <Route path='/signup' element={<SignUp />} />
+      <Route path='/signout' element={<Signout />} />
+      <Route path='/profile' element={<Profile />} />
+      <Route path='/about' element={<About />} />
+    </Routes>
+  </BrowserRouter>
   )
 }
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { FaSearch } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+export default function Header() {
+    return (
+        <header className='bg-slate-200 shadow-md'>
+            <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+                <Link to='/'>
+                    <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+                        <sapn>TO.</sapn>
+                        <sapn className='text-green-700'>Estate</sapn>
+                    </h1>
+                </Link>
+                <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+                    <input type='text' placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64' /><FaSearch className='hover:cursor-pointer hover:text-green-700' />
+                </form>
+
+                <ul className='flex gap-4'>
+                    <Link to='/'>
+                        <li className='hidden sm:inline text-slate-700  hover:text-green-700 hover:cursor-pointer'>Home</li>
+                    </Link>
+                    <Link to='/About'>
+                        <li className='hidden sm:inline text-slate-700  hover:text-green-700 hover:cursor-pointer'>About</li>
+                    </Link>
+                    <Link to='/Signin'>
+                        <li className='hidden sm:inline text-slate-700  hover:text-green-700 hover:cursor-pointer'>Signin</li>
+                    </Link>
+                </ul>
+            </div>
+        </header >
+    )
+}
