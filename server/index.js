d8d5c2e0962aec8f9a93d4e386a691ac2de2fd83
ismail01-jl <jellitiismail2002@gmail.com>
2024-07-08 20:23:19 +0100

    Create and run the server

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from 'express'
+ const app=express()
+  app.listen(3007,()=>{
+    console.log('server is running')
+  })
+/*import mongoose from 'mongoose';
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// data base connecting
+mongoose.connect(process.env.DATABASE)
+    .then(() => { console.log("DataBase Successfully Connected"); })
+    .catch(err => {
+        console.log("Unable to connect to database", err);
+        process.exit();
+    });
+const app = express();
+// requête 
+app.get("/", (req, res) => {
+    res.send("hello si ismail");
+});
+app.listen(process.env.PORT, () => {
+    console.log(`Server is listening on port ${process.env.PORT}`);
+});*/
