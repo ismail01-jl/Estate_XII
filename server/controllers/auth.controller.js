47550862f356c956a829a62fe9ddc29407e71c5a
ismail01-jl <jellitiismail2002@gmail.com>
2024-07-10 18:59:32 +0100

    Create Signin API route

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,8 +1,9 @@
 import User from "../models/user.model.js";
-//import {errorHandler} from "../utils/error.js"
+import { errorHandler } from "../utils/error.js"
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
-export const signup = async (req, res ,next) => {
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10)
     const newuser = new User({ username, email, password: hashedPassword });
@@ -11,5 +12,20 @@ export const signup = async (req, res ,next) => {
         res.status(201).json("newuser created succsessfully");
     } catch (error) {
         next(error);
+    }
+}
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email: email });
+        if (!user) return next(errorHandler(404, 'user not found!!'))
+        const validpassword = bcryptjs.compareSync(password, user.password);
+        if (!validpassword) return next(errorHandler(405, 'check password or email !!'));
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+        const {password: pass , ...rest} = user._doc
+        res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest);
+
+    } catch (error) {
+        next(error)
     }
 }
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,7 +1,8 @@
 import express from 'express'
-const router=express.Router();
-import { signup } from '../controllers/auth.controller.js';
+const router = express.Router();
+import { signup , signin } from '../controllers/auth.controller.js';
 
-router.post("/signup", signup )
+router.post("/signup", signup)
+router.post("/signin", signin)
 
-export default router ;
+export default router;
