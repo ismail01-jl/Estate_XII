3b2a54a86b51c96effcb88ebb40111b24ebae4b8
ismail01-jl <jellitiismail2002@gmail.com>
2024-08-01 18:21:27 +0100

    Create Search API route

--- server/routes/listing.route.js
+++ server/routes/listing.route.js
@@ -1,10 +1,11 @@
 import express from 'express'
 const router = express.Router();
-import { createListing, getListings ,deleteListing , updateListing } from '../controllers/listing.controller.js'
+import { createListing, getListings , getSearchListing ,deleteListing , updateListing } from '../controllers/listing.controller.js'
 import { verifyToken } from '../utils/VerfiyUser.js';
 
 router.post('/create', verifyToken, createListing)
 router.delete('/delete/:id', verifyToken, deleteListing)
 router.put('/update/:id', verifyToken, updateListing )
 router.get('/getListing/:id', getListings)
+router.get('/get', getSearchListing)
 export default router;
