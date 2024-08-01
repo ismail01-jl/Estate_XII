7724b29b48d74acd39a85336dc007e4f4aa80dfb
ismail01-jl <jellitiismail2002@gmail.com>
2024-08-01 19:27:00 +0100

    Complete header search form functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
 import { FaSearch } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import { useEffect, useState } from 'react';
 
 export default function Header() {
     const { currentuser } = useSelector(state => state.user)
+    const [searchTerm, setSearchTerm] = useState('');
+    const Navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const urlParams = new URLSearchParams(window.location.search);
+        urlParams.set('searchTerm', searchTerm);
+        const searchQuery = urlParams.toString();
+        Navigate(`/search?${searchQuery}`)
+    }
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const searchTermFromUrl = urlParams.get('searchTerm');
+        if (searchTermFromUrl) {
+            setSearchTerm(searchTermFromUrl);
+        }
+    }, [location.search]);
     return (
         <header className='bg-slate-200 shadow-md'>
             <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -14,8 +31,17 @@ export default function Header() {
                         <sapn className='text-green-700'>Estate</sapn>
                     </h1>
                 </Link>
-                <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
-                    <input type='text' placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64' /><FaSearch className='hover:cursor-pointer hover:text-green-700' />
+                <form onSubmit={handleSubmit} className='bg-slate-100 p-3 rounded-lg flex items-center'>
+                    <input type='text'
+                        placeholder='Search...'
+                        className='bg-transparent focus:outline-none w-24 sm:w-64'
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                    />
+                    <button>
+                        <FaSearch className='hover:cursor-pointer hover:text-green-700' />
+                    </button>
+
                 </form>
 
                 <ul className='flex gap-4'>
