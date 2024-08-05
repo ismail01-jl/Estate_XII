1aafbc1fe84da7bff5fcb6c5f7f89ff6ceecee62
ismail01-jl <jellitiismail2002@gmail.com>
2024-08-05 18:49:48 +0100

    Add show more listings functionality

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -15,7 +15,8 @@ export default function Search() {
     });
     const [loading, setLoading] = useState(false);
     const [listings, setListings] = useState([]);
-    //const [showMore, setShowMore] = useState(false);
+    const [showMore, setShowMore] = useState(false);
+
     //console.log(listings)
     useEffect(() => {
         const urlParams = new URLSearchParams(location.search);
@@ -47,15 +48,15 @@ export default function Search() {
         }
         const fetchListings = async () => {
             setLoading(true);
-            //setShowMore(false);
+            setShowMore(false);
             const searchQuery = urlParams.toString();
             const res = await fetch(`/api/listing/get?${searchQuery}`);
             const data = await res.json();
-            /*if (data.length > 8) {
-                   setShowMore(true);
-               } else {
-                   setShowMore(false);
-               }*/
+            if (data.length > 8) {
+                setShowMore(true);
+            } else {
+                setShowMore(false);
+            }
             setListings(data);
             setLoading(false);
         };
@@ -97,6 +98,20 @@ export default function Search() {
         const searchQuery = urlParams.toString();
         navigate(`/search?${searchQuery}`);
     }
+
+    const onShowMoreClick = async () => {
+        const numberOfListings = listings.length;
+        const startIndex = numberOfListings;
+        const urlParams = new URLSearchParams(location.search);
+        urlParams.set('startIndex', startIndex);
+        const searchQuery = urlParams.toString();
+        const res = await fetch(`/api/listing/get?${searchQuery}`);
+        const data = await res.json();
+        if (data.length < 9) {
+            setShowMore(false);
+        }
+        setListings([...listings, ...data]);
+    };
     return (
         <div className='flex flex-col md:flex-row'>
             <div className='p-6 border-b-2 md:border-r-2 md:min-h-screen'>
@@ -214,6 +229,14 @@ export default function Search() {
                         listings.map((listing) => (
                             <ListingsCards key={listing._id} listing={listing} />
                         ))}
+                    {showMore && (
+                        <button
+                            onClick={() => { onShowMoreClick(); }}
+                            className=' text-blue-700 text-sm hover:underline p-7 text-center w-full'
+                        >
+                            Show more
+                        </button>
+                    )}
                 </div>
             </div>
         </div>
